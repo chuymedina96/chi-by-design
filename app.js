2bb18bd3cafcf6c64d20a2ed31dd77bdff856204
Chuy Medina <chuymedina96@gmail.com>
2018-01-27 13:02:41 -0600

    added CNAME, creating mroe views for actual page after landing page

--- app.js
+++ app.js
@@ -6,9 +6,18 @@ var express 	= require("express"),
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
-	res.render("landing.ejs");
+	res.render("landing");
 });
+app.get("/chibydesign", function(req, res){
+	res.render("chibydesign/index");
+});
+app.get("*", function(req, res){
+	res.render("404");
+});
+
 app.listen(process.env.PORT || 3000, function(){
 	console.log("Server has started");
 });

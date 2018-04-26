b12c261791c6ed2199b38366ed7efa45928ad4d1
Chuy Medina <chuymedina96@gmail.com>
2018-04-26 15:12:37 -0500

    compressed images and some css files

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
 var express 	= require("express"),
-	app			= express(),
-	bodyParser	= require("body-parser"),
-	mongoose	= require("mongoose");
-
+	app			= express();
 //Routes
 var indexRoutes = require("./routes/index.js");
-
 //Body parser and express dir
-app.use(bodyParser.urlencoded({extended: true}));
+//app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 //Templating engine
 app.set("view engine", "ejs");
-
 app.use("/", indexRoutes);
-
 app.listen(process.env.PORT || 3000, function(){
 	console.log("Server has started");
 });
